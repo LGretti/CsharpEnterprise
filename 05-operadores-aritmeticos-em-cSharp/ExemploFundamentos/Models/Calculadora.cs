ae979fad372a53827aa6582d4a802033f2fde4ee
Lucas Gretti <lucassgretti@outlook.com>
2023-02-13 15:46:52 -0300

    Funcoes Trigonometricas

--- 05-operadores-aritmeticos-em-cSharp/ExemploFundamentos/Models/Calculadora.cs
+++ 05-operadores-aritmeticos-em-cSharp/ExemploFundamentos/Models/Calculadora.cs
@@ -22,5 +22,23 @@ namespace ExemploFundamentos.Models {
         public void Potencia(int x, int y) {
             Console.WriteLine($"{x}^{y} = {Math.Pow(x, y)}");
         }
+
+        public void Seno(double angulo) {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° {Math.Round(seno,4)}");
+        }
+
+        public void Coseno(double angulo) {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo}° = {Math.Round(coseno,4)}");
+        }
+
+        public void Tangente(double angulo) {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo}° = {Math.Round(tangente,4)}");
+        }
     }
 }
