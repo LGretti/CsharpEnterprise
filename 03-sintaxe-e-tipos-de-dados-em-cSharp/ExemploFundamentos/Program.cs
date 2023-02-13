24966ee2586f2120b112df5dd2453f68325c32a4
Lucas Gretti <lucassgretti@outlook.com>
2023-02-12 23:41:54 -0300

    inicial

---
+++ 03-sintaxe-e-tipos-de-dados-em-cSharp/ExemploFundamentos/Program.cs
@@ -0,0 +1,48 @@
+﻿using ExemploFundamentos.Models;
+
+string apresentacao = "Olá, seja bem vindo";
+
+//Pega a hora da maquina que a app ta rodando com o mesmo formato
+//DateTime dataAtual = DateTime.Now;
+
+DateTime dataAtual = DateTime.Now.AddDays(1);
+
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm:ss"));
+
+
+
+
+
+
+
+/*
+int quantidade = 1;
+Console.WriteLine("O valor da variável quantidade é:" + quantidade);
+
+quantidade = 10;
+Console.WriteLine("O valor da variável quantidade é:" + quantidade);
+*/
+
+/*
+double altura = 1.8;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("O valor da variável quantidade é:" + quantidade);
+Console.WriteLine("O valor da variável altura é:" + altura.ToString("0.00"));
+Console.WriteLine("O valor da variável preco é:" + preco);
+Console.WriteLine("O valor da variável condicao é:" + condicao);
+*/
+
+/*Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 26;
+pessoa1.Apresentar();
+
+Pessoa pessoa_FisicaRepresentacao = new Pessoa();
+
+*/
---
+++ 04-tipos-de-operadores-em-cSharp/ExemploFundamentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExemploFundamentos.Models;
+
+int quantidadeEmEstoque = 10;
+
+int quantidadeCompra = 4; 
+
+if (quantidadeEmEstoque >= quantidadeCompra) {
+    Console.WriteLine("Venda Realizada");
+} else {
+    Console.WriteLine("Sem estoque suficiente.");
+}
+
+
+
+
+/*int a = 5;
+
+Console.WriteLine(a);*/
