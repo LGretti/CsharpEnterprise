004594cd62f12fc1d48c760c230fa70aeb17213b
Lucas Gretti <lucassgretti@outlook.com>
2023-02-13 15:48:46 -0300

    Incremento e decremento

--- 05-operadores-aritmeticos-em-cSharp/ExemploFundamentos/Program.cs
+++ 05-operadores-aritmeticos-em-cSharp/ExemploFundamentos/Program.cs
@@ -3,3 +3,13 @@
 Calculadora calc = new Calculadora();
 
 calc.Potencia(3,2);
+
+int incremento = 10;
+
+
+incremento += 1;
+Console.WriteLine(incremento);
+incremento++;
+Console.WriteLine(incremento);
+incremento--;
+Console.WriteLine(incremento);
