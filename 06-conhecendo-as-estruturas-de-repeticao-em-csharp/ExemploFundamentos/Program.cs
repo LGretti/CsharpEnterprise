ac36570b46dabea930a3a62858114234c730fd50
Lucas Gretti <lucassgretti@outlook.com>
2023-02-13 22:16:49 -0300

    do while

--- 06-conhecendo-as-estruturas-de-repeticao-em-csharp/ExemploFundamentos/Program.cs
+++ 06-conhecendo-as-estruturas-de-repeticao-em-csharp/ExemploFundamentos/Program.cs
@@ -1,19 +1,28 @@
 ﻿using ExemploFundamentos.Models;
 
-int numero = 5;
+int numero = 0;
 int contador = 0;
 
-while (contador <= 10) {
+do {
+    Console.WriteLine("Digite um número (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    contador += numero;
+} while (numero != 0);
+
+Console.WriteLine($"soma = {contador}");
+
+
+
+
+/*while (contador <= 10) {
     Console.WriteLine($"{contador + 1}º Execução: {numero} x {contador} = {numero * contador}");
     contador++;
 
     if (contador == 6) {
         break;
     }
-}
-
-
-
+}*/
 
 /*for (int x = 1; x <= 10; x++) {
     Console.WriteLine($"------{x}------");
