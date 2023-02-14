9526ab3bc1a7e9d18dbb9e171708570436b36ef1
Lucas Gretti <lucassgretti@outlook.com>
2023-02-13 22:02:27 -0300

    Repeticoes FOR

---
+++ 06-conhecendo-as-estruturas-de-repeticao-em-csharp/ExemploFundamentos/Program.cs
@@ -0,0 +1,9 @@
+﻿using ExemploFundamentos.Models;
+
+for (int x = 1; x <= 10; x++) {
+    Console.WriteLine($"------{x}------");
+    for (int y = 1; y <= 10; y++) {
+        Console.WriteLine($"{x} x {y} = {x * y}");
+    }
+}
+
