e0c7dba41714a59858e1a86228f8397937352640
Lucas Gretti <lucassgretti@outlook.com>
2023-02-13 22:08:30 -0300

    comando WHILE

--- 06-conhecendo-as-estruturas-de-repeticao-em-csharp/ExemploFundamentos/Program.cs
+++ 06-conhecendo-as-estruturas-de-repeticao-em-csharp/ExemploFundamentos/Program.cs
@@ -1,9 +1,21 @@
 ﻿using ExemploFundamentos.Models;
 
-for (int x = 1; x <= 10; x++) {
+int numero = 5;
+int contador = 0;
+
+while (contador <= 10) {
+    Console.WriteLine($"{contador + 1}º Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+}
+
+
+
+
+/*for (int x = 1; x <= 10; x++) {
     Console.WriteLine($"------{x}------");
     for (int y = 1; y <= 10; y++) {
         Console.WriteLine($"{x} x {y} = {x * y}");
     }
 }
 
+*/
