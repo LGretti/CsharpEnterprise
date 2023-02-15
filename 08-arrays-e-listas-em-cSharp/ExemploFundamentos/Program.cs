c23c72fbca9c67a87178e7042ccfb4f1e133e139
Lucas Gretti <lucassgretti@outlook.com>
2023-02-15 14:09:38 -0300

    redimensionando arrays

--- 08-arrays-e-listas-em-cSharp/ExemploFundamentos/Program.cs
+++ 08-arrays-e-listas-em-cSharp/ExemploFundamentos/Program.cs
@@ -7,14 +7,33 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 //arrayInteiros[3] = 1;
 
+//qual array, qual tamanho
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
 Console.WriteLine("Array com For");
 for (int i = 0; i < arrayInteiros.Length; i++) {
     Console.WriteLine($"Posição n{i} - {arrayInteiros[i]}");
 }
 
-Console.WriteLine("Array com Foreach que não pode acessar o contador");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Console.WriteLine("Array com Foreach que não pode acessar o contador");
 int con = 0;
 foreach (int valor in arrayInteiros) {
     Console.WriteLine(valor + " " + con);
     con++;
-}
+}*/
