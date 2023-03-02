d0339de7b282beca739ae1ea05dd99ebaf21b03a
Lucas Gretti <lucassgretti@outlook.com>
2023-03-02 11:07:01 -0300

    exercicio2 -pink e cerebro

--- 11-desafios-basicos-de-cSharp/desafios/Program.cs
+++ 11-desafios-basicos-de-cSharp/desafios/Program.cs
@@ -24,7 +24,28 @@ void exercicio1() {
 };
 
 void exercicio2() {
+    int x = int.Parse(Console.ReadLine());
+    string[] n = Console.ReadLine().Split(' ');
 
+    int multiplo2 = 0;
+    int multiplo3 = 0;
+    int multiplo4 = 0;
+    int multiplo5 = 0;
+
+    for (int i = 0; i < x; i++) {
+        if ((int.Parse(n[i]) % 2) == 0) multiplo2++;
+
+        if ((int.Parse(n[i]) % 3) == 0) multiplo3++;
+
+        if ((int.Parse(n[i]) % 4) == 0) multiplo4++;
+
+        if ((int.Parse(n[i]) % 5) == 0) multiplo5++;
+    }
+
+    Console.WriteLine("{0} Multiplo(s) de 2", multiplo2);
+    Console.WriteLine("{0} Multiplo(s) de 3", multiplo3);
+    Console.WriteLine("{0} Multiplo(s) de 4", multiplo4);
+    Console.WriteLine("{0} Multiplo(s) de 5", multiplo5);
 }
 
 void exercicio3() {
