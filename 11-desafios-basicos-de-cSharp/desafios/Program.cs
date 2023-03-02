4fc8ab7e12b2955568b57d0bc523faa883e651fc
Lucas Gretti <lucassgretti@outlook.com>
2023-03-02 10:55:10 -0300

    desafios basicos 01

---
+++ 11-desafios-basicos-de-cSharp/desafios/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string op = Console.ReadLine();
+
+switch (op) {
+    case "1":
+        exercicio1();
+        break;
+    case "2":
+        exercicio2();
+        break;
+    case "3":
+        exercicio3();
+        break;
+    default:
+        break;
+}
+
+void exercicio1() {
+    int a, b;
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("PROD = " + (a * b)); //implemente o código que representa a multiplicação.
+    Console.ReadLine();
+};
+
+void exercicio2() {
+
+}
+
+void exercicio3() {
+
+}
