11531689a2dcf41a692cc2b5f48841e9901a5c14
Lucas Gretti <lucassgretti@outlook.com>
2023-03-12 20:41:19 -0300

    get e set

--- 13-propriedades-metodos-e-construtores-com-cSharp/props-metodos-e-construtores/Pessoa.cs
+++ 13-propriedades-metodos-e-construtores-com-cSharp/props-metodos-e-construtores/Pessoa.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace props_metodos_e_construtores {
     public class Pessoa {
-        public string Nome { get; set; }
+        private string _nome;
+
+        public string Nome {
+            get { 
+                return _nome.ToUpper();
+            }
+
+            set {
+                if ((value == "") || (value == null)) {
+                    throw new ArgumentException("Nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
 
         public int Idade { get; set; }
+
+        public void Apresentar() {
+            Console.WriteLine($"Olá, me chamo {Nome}.");
+        }
     }
 }
--- 13-propriedades-metodos-e-construtores-com-cSharp/props-metodos-e-construtores/Program.cs
+++ 13-propriedades-metodos-e-construtores-com-cSharp/props-metodos-e-construtores/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using props_metodos_e_construtores;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Lucas";
