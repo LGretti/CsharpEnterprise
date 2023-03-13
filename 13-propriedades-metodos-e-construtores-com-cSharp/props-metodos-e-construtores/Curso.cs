30451827ddd7dd61954239c1427cec00c20efdc7
Lucas Gretti <lucassgretti@outlook.com>
2023-03-12 21:10:04 -0300

    impl classe curso

---
+++ 13-propriedades-metodos-e-construtores-com-cSharp/props-metodos-e-construtores/Curso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace props_metodos_e_construtores {
+    public class Curso {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno) {
+            Alunos.Add(aluno);
+        }
+    }
+}
