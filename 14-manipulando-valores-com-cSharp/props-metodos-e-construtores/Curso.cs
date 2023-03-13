ff3f2d2f7f044262f978ad5f7653ed94768e8307
Lucas Gretti <lucassgretti@outlook.com>
2023-03-13 09:43:56 -0300

    concatenando strings

---
+++ 14-manipulando-valores-com-cSharp/props-metodos-e-construtores/Curso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace props_metodos_e_construtores {
+    public class Curso {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno) {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados() {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+
+        public bool RemoverAluno(Pessoa aluno) {
+            return Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos() {
+            for (int count = 0; count < Alunos.Count; count++) {
+                string texto = "No. " + count + " " + Alunos[count].NomeCompleto;
+                Console.WriteLine(texto);
+            }
+        }
+    }
+}
---
+++ 14-manipulando-valores-com-cSharp/props-metodos-e-construtores/Program.cs
@@ -0,0 +1,17 @@
+﻿using props_metodos_e_construtores;
+
+Pessoa p1 = new Pessoa(nome: "Lucas", sobrenome: "Gretti");
+//p1.Nome = "Lucas";
+//p1.Sobrenome = "Gretti";
+
+Pessoa p2 = new Pessoa("João", "Silva");
+//p1.Nome = "Joao";
+//p1.Sobrenome = "Silva";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
