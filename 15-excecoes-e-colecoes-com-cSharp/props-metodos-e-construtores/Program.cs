23e951fec68aca97ce6c276ff1e40f8a6351b5f8
Lucas Gretti <lucassgretti@outlook.com>
2023-03-29 22:14:44 -0300

    usando trhow

--- 15-excecoes-e-colecoes-com-cSharp/props-metodos-e-construtores/Program.cs
+++ 15-excecoes-e-colecoes-com-cSharp/props-metodos-e-construtores/Program.cs
@@ -2,21 +2,4 @@
 using System.Globalization;
 
 
-try {
-    string[] linhas = File.ReadAllLines("files/testaae.txt");
-
-    foreach (string line in linhas) {
-        Console.WriteLine(line);
-    }
-} catch(FileNotFoundException errMsg) { //Começa pelas tratativas específicas e termina na generalista
-    Console.WriteLine($"Arquivo não encontrado: {errMsg}");
-
-} catch (DirectoryNotFoundException errMsg) {
-    Console.WriteLine($"Diretório não encontrado: {errMsg}");
-
-} catch(Exception errMsg) { //Mais geral, todas vêm daqui
-    Console.WriteLine($"Ocorreu um erro: {errMsg}");
-} finally { //Finalmente (executa dando erro ou não)
-    Console.WriteLine("Chegou até aqui");
-}
-
+new ExemploExcecao().Metodo1();
