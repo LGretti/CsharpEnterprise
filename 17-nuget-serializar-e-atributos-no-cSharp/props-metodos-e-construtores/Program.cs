5c47bfa9d983534cbe36336b4f6a4c29093110fa
Lucas Gretti <lucassgretti@outlook.com>
2023-04-11 17:25:26 -0300

    serializando listas em json

--- 17-nuget-serializar-e-atributos-no-cSharp/props-metodos-e-construtores/Program.cs
+++ 17-nuget-serializar-e-atributos-no-cSharp/props-metodos-e-construtores/Program.cs
@@ -2,14 +2,18 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
-//transforamndo essa venda em json
+
+List<Venda> listaVendas = new List<Venda>();
+
+
 Venda venda1 = new Venda(1, "materiais de esc", 25.00M);
+Venda venda2 = new Venda(1, "licenca software", 2500.00M);
 
-//metodo SerializeObject - ler documentacao
-//ele transofrma esse objeto em uma string json
-string serializado = JsonConvert.SerializeObject(venda1, Formatting.Indented);
+listaVendas.Add(venda1);
+listaVendas.Add(venda2);
 
-//Escreve todo o texto em um arquivo json
-File.WriteAllText("files/vendas.json", serializado);
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+//File.WriteAllText("files/vendas.json", serializado);
 
 Console.WriteLine(serializado);
