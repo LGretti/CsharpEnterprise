f08a2839a569b138151a702ce44ff57b0b00a0f6
Lucas Gretti <lucassgretti@outlook.com>
2023-04-11 09:04:04 -0300

    tuplas cap novo

---
+++ 16-tuplas-operador-ternario-e-desconstrucao-de-um-objeto-com-cSharp/props-metodos-e-construtores/Program.cs
@@ -0,0 +1,10 @@
+﻿using props_metodos_e_construtores;
+using System.Globalization;
+
+//id, nome   sobrenome
+(int, string, string, decimal) tupla = (1, "Lucas", "Gretti", 1.78M);
+
+Console.WriteLine($"ID: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}");
