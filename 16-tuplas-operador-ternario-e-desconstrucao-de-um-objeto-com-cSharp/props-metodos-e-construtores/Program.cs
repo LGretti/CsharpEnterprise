e04f9651f979dcd18d3080cec61cb280d86890a5
Lucas Gretti <lucassgretti@outlook.com>
2023-04-11 09:51:13 -0300

    tuplas

--- 16-tuplas-operador-ternario-e-desconstrucao-de-um-objeto-com-cSharp/props-metodos-e-construtores/Program.cs
+++ 16-tuplas-operador-ternario-e-desconstrucao-de-um-objeto-com-cSharp/props-metodos-e-construtores/Program.cs
@@ -1,10 +1,17 @@
 ﻿using props_metodos_e_construtores;
 using System.Globalization;
 
-//id, nome   sobrenome
-(int, string, string, decimal) tupla = (1, "Lucas", "Gretti", 1.78M);
+//melhor forma
+(int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Lucas", "Gretti", 1.78M);
 
-Console.WriteLine($"ID: {tupla.Item1}");
-Console.WriteLine($"Nome: {tupla.Item2}");
-Console.WriteLine($"Sobrenome: {tupla.Item3}");
-Console.WriteLine($"Altura: {tupla.Item4}");
+//outra forma, so declara o tipo de cada item
+ValueTuple<int, string, string, decimal> tupla2 = (1, "Lucas", "Gretti", 1.78M);
+
+//outra forma, so coloca o valor, ele pega o tipo sozinho
+var tupla3 = Tuple.Create(1, "Lucas", "Gretti", 1.78M);
+
+
+Console.WriteLine($"ID: {tupla.Id}");
+Console.WriteLine($"Nome: {tupla.Nome}");
+Console.WriteLine($"Sobrenome: {tupla.Sobrenome}");
+Console.WriteLine($"Altura: {tupla.Altura}");
