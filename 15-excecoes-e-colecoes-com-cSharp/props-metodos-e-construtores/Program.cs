97128dc8a75a1fa2c818a9e9fbfaf0359b7d186e
Lucas Gretti <lucassgretti@outlook.com>
2023-04-10 22:58:35 -0300

    fila na pratica

--- 15-excecoes-e-colecoes-com-cSharp/props-metodos-e-construtores/Program.cs
+++ 15-excecoes-e-colecoes-com-cSharp/props-metodos-e-construtores/Program.cs
@@ -2,4 +2,24 @@
 using System.Globalization;
 
 
-new ExemploExcecao().Metodo1();
+
+Queue<int> fila = new Queue<int>();
+
+//adicionar elemento na fila na ultima posicao
+fila.Enqueue(1);
+fila.Enqueue(2);
+fila.Enqueue(3);
+fila.Enqueue(4);
+
+foreach (int i in fila) {
+    Console.WriteLine(i);
+}
+
+//remove sempre o PRIMEIRO ELEMENTO da fila
+fila.Dequeue();
+
+Console.WriteLine("");
+Console.WriteLine("dequeue");
+foreach (int i in fila) {
+    Console.WriteLine(i);
+}
