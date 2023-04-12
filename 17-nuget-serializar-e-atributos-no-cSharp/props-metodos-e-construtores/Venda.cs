f12f47851c27d2c38741e44be597f53a0f91cf17
Lucas Gretti <lucassgretti@outlook.com>
2023-04-11 22:07:02 -0300

    Atributos de propriedades

--- 17-nuget-serializar-e-atributos-no-cSharp/props-metodos-e-construtores/Venda.cs
+++ 17-nuget-serializar-e-atributos-no-cSharp/props-metodos-e-construtores/Venda.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace props_metodos_e_construtores {
     public class Venda {
         public int Id { get; set; }
+
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
+
         public decimal Preco { get; set; }
+
         public DateTime DataVenda { get; set; }
 
 
