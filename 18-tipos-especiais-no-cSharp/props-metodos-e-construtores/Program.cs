e20b69372769e806ccde57f231df3f393dc17b73
Lucas Gretti <lucassgretti@outlook.com>
2023-04-12 12:27:04 -0300

    tipo anonimo (?)

--- 18-tipos-especiais-no-cSharp/props-metodos-e-construtores/Program.cs
+++ 18-tipos-especiais-no-cSharp/props-metodos-e-construtores/Program.cs
@@ -4,14 +4,32 @@ using Newtonsoft.Json;
 using Microsoft.VisualBasic;
 using System.ComponentModel.Design;
 
-string conteudoArquivo = File.ReadAllText("files/vendas.json");
 
-List<Venda>? listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+var tipoAnonimo = { Nome = "Lucas", Sobrenome = "Gretti", Altura = 1.78 };
 
-foreach (Venda venda in listaVenda) {
-    Console.WriteLine($"Venda Id. {venda.Id} - " +
-                      $"Produto: {venda.Produto} - " +
-                      $"R${venda.Preco} - " +
-                      $"Data: {venda.DataVenda.ToString("dd/MM/yyyy")} - " +
-                      $"{(venda.Desconto.HasValue ? $"Desconto: {venda.Desconto}" : "")}");
-}
+Console.WriteLine(tipoAnonimo.Nome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string conteudoArquivo = File.ReadAllText("files/vendas.json");
+
+//List<Venda>? listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+//foreach (Venda venda in listaVenda) {
+//    Console.WriteLine($"Venda Id. {venda.Id} - " +
+//                      $"Produto: {venda.Produto} - " +
+//                      $"R${venda.Preco} - " +
+//                      $"Data: {venda.DataVenda.ToString("dd/MM/yyyy")} - " +
+//                      $"{(venda.Desconto.HasValue ? $"Desconto: {venda.Desconto}" : "")}");
+//}
