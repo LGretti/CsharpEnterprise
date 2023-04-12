d841f5f89257c24d50fa858f1b0a0d55638517d2
Lucas Gretti <lucassgretti@outlook.com>
2023-04-11 22:02:05 -0300

    deserializando objetos

--- 17-nuget-serializar-e-atributos-no-cSharp/props-metodos-e-construtores/Program.cs
+++ 17-nuget-serializar-e-atributos-no-cSharp/props-metodos-e-construtores/Program.cs
@@ -1,11 +1,22 @@
 ﻿using props_metodos_e_construtores;
 using System.Globalization;
 using Newtonsoft.Json;
+using Microsoft.VisualBasic;
 
-DateTime dataAtual = DateTime.Now;
+string conteudoArquivo = File.ReadAllText("files/vendas.json");
 
-List<Venda> listaVendas = new List<Venda>();
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
+foreach (Venda venda in listaVenda) {
+    Console.WriteLine($"Venda Id. {venda.Id} - " +
+                      $"Produto: {venda.Produto} - " +
+                      $"R${venda.Preco} - " +
+                      $"Data: {venda.DataVenda.ToString("dd/MM/yyyy")}");
+}
+
+/*List<Venda> listaVendas = new List<Venda>();
+
+//DateTime dataAtual = DateTime.Now;
 
 Venda venda1 = new Venda(1, "materiais de esc", 25.00M, dataAtual);
 Venda venda2 = new Venda(1, "licenca software", 2500.00M, dataAtual);
@@ -17,4 +28,4 @@ string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indente
 
 File.WriteAllText("files/vendas.json", serializado);
 
-Console.WriteLine(serializado);
+Console.WriteLine(serializado);*/
