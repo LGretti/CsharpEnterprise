f50d38887e904f6b13ecc277e2c9b053376ee9a0
Lucas Gretti <lucassgretti@outlook.com>
2023-04-13 08:22:37 -0300

    tipos de referência

--- 19-stack-heap-e-garbage-collector-no-cSharp/props-metodos-e-construtores/Program.cs
+++ 19-stack-heap-e-garbage-collector-no-cSharp/props-metodos-e-construtores/Program.cs
@@ -5,14 +5,52 @@ using Microsoft.VisualBasic;
 using System.ComponentModel.Design;
 
 
-int numero = 30;
-bool par = false;
 
-//if ternario
-//variavel int numero recebeu agora esse EhPar();
-par = numero.EhPar();
+Pessoa p1 = new Pessoa("Lucas", "gretti");
+Pessoa p2 = p1;
 
-string mensagem = "O número " + numero + " é " + (par ? "par" : "impar");
+//o p1 e o p2 estão como Vinicius gretti
+p2.Nome = "Vinicius";
+
+
+Console.WriteLine(p1.NomeCompleto);
+Console.WriteLine(p2.NomeCompleto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int numero = 30;
+//bool par = false;
+
+////if ternario
+////variavel int numero recebeu agora esse EhPar();
+//par = numero.EhPar();
+
+//string mensagem = "O número " + numero + " é " + (par ? "par" : "impar");
 
 
 
