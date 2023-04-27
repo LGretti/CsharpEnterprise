90fdff043fe72ecffb8df16c38ac09ff7d02f151
Lucas Gretti <lucassgretti@outlook.com>
2023-04-27 18:16:36 -0300

    using min, max e avg

--- 25-SQL-SERVER-manipulando-dados/aulas.sql
+++ 25-SQL-SERVER-manipulando-dados/aulas.sql
@@ -8,3 +8,16 @@ SELECT count(*) as QuantidadeProdutosMedios FROM Produtos where tamanho = 'M'
 --sum
 
 SELECT sum(preco) as precototalprodutosmedios FROM Produtos where tamanho = 'M'
+
+
+--min
+
+select min(preco) from produtos
+
+--max
+
+select max(preco) from produtos
+
+--avg
+
+select avg(preco) from produtos
