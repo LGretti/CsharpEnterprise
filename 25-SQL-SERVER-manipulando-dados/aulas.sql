31dd1d6d813941493c3a4647a102c817dcfd97ad
Lucas Gretti <lucassgretti@outlook.com>
2023-04-27 18:10:14 -0300

    using sum

--- 25-SQL-SERVER-manipulando-dados/aulas.sql
+++ 25-SQL-SERVER-manipulando-dados/aulas.sql
@@ -5,3 +5,6 @@ SELECT count(*) as QuantidadeProdutos FROM Produtos
 SELECT count(*) as QuantidadeProdutosMedios FROM Produtos where tamanho = 'M'
 
 
+--sum
+
+SELECT sum(preco) as precototalprodutosmedios FROM Produtos where tamanho = 'M'
