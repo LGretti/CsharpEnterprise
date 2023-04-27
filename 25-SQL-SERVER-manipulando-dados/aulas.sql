79cc2c831a8ad1a6c82aaba718f666c5083b8f8b
Lucas Gretti <lucassgretti@outlook.com>
2023-04-27 19:56:09 -0300

    foregin key

--- 25-SQL-SERVER-manipulando-dados/aulas.sql
+++ 25-SQL-SERVER-manipulando-dados/aulas.sql
@@ -44,3 +44,30 @@ from produtos
 alter table produtos
 add DataCadastro datetime
 update produtos set DataCadastro = getdate()
+
+--format
+
+select format(datacadastro, 'dd-MM-yyyy') as data from produtos
+
+--group by
+
+select tamanho,
+       count(*) as quantidade
+from produtos
+where tamanho <> ''
+group by tamanho
+
+-- adicionando foreign key
+CREATE TABLE Enderecos (
+	Id int Primary key identity(1,1) not null,
+	IdCliente int null,
+	Rua varchar(255) null,
+	Bairro varchar(255) null,
+	Cidade varchar(255) null,
+	Estado char(2) null
+
+constraint FK_Enderecos_Clientes FOREIGN KEY(IDCliente)
+REFERENCES clientes(id))
+
+
+select * from clientes
