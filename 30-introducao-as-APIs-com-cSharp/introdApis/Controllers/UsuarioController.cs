f9c2f540b1d9fc52e3e30a32524f2967fe07ab72
Lucas Gretti <lucassgretti@outlook.com>
2023-05-21 14:28:13 -0300

    iniciando o crud

---
+++ 30-introducao-as-APIs-com-cSharp/introdApis/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace introdApis.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase {
+
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora() {
+            var obj = new {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar (string nome) {
+            var msg = $"Olá {nome}, seja bem vindo(a)!";
+
+            return Ok(new { msg });
+        }
+    }
+}
