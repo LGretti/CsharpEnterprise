0366dc7919a64a8d21d756c25d85251a3d6f1616
Lucas Gretti <lucassgretti@outlook.com>
2023-06-13 23:42:15 -0300

    finalizando agenda context e nullable no prj

--- 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Context/AgendaContext.cs
+++ 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Context/AgendaContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC;
 public class AgendaContext : DbContext {
-  public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) { }
+    // Construtor que passa a opção genérica para a classe base DbContext
+    public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) { }
 
-  
+    //propriedade DBset(tabela) - como se a Agenda fosse a base e o dbset a tabela
+    public DbSet<Contato> Contatos { get; set; }
 
 }
