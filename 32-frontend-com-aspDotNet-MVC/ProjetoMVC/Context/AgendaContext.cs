f156fb70a2d10e3441d7c9410f390b2a213c05af
Lucas Gretti <lucassgretti@outlook.com>
2023-06-13 22:34:58 -0300

    criando Model Contato e inicio Agenda Context

---
+++ 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Context/AgendaContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoMVC;
+public class AgendaContext : DbContext {
+  public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) { }
+
+  
+
+}
