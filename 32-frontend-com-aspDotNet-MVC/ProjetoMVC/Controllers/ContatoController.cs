60e29281328149682b02063b744a2df2a9a34199
Lucas Gretti <lucassgretti@outlook.com>
2023-06-18 02:29:57 -0300

    controller com view index

---
+++ 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Controllers/ContatoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoMVC;
+public class ContatoController : Controller
+{
+  //Listar todos os contatos no banco
+  public IActionResult Index(){
+    return View();
+  }
+}
