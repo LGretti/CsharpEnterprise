57b130e9379d06691e52d0d26f4e438a4c28e838
Lucas Gretti <lucassgretti@outlook.com>
2023-07-18 16:58:50 -0300

    Deletar, tela, get e post

--- 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Controllers/ContatoController.cs
+++ 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Controllers/ContatoController.cs
@@ -59,4 +59,23 @@ public class ContatoController : Controller
 
     return View(contato);
   }
+
+  public IActionResult Deletar(int id) {
+    var contato = _context.Contatos.Find(id);
+    
+    if (contato == null)
+      return RedirectToAction(nameof(Index));
+
+    return View(contato);
+  }
+
+  [HttpPost]
+  public IActionResult Deletar(Contato contato){
+    Contato contatoBD =  _context.Contatos.Find(contato.Id);
+
+    _context.Contatos.Remove(contatoBD);
+    _context.SaveChanges();
+
+    return RedirectToAction(nameof(Index));
+  }
 }
