cd14e7d0e46abe3df06e8b795c1c4df4c0ae6832
Lucas Gretti <lucassgretti@outlook.com>
2023-07-18 16:49:07 -0300

    Detalhes do contato

--- 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Controllers/ContatoController.cs
+++ 32-frontend-com-aspDotNet-MVC/ProjetoMVC/Controllers/ContatoController.cs
@@ -50,4 +50,13 @@ public class ContatoController : Controller
 
     return RedirectToAction(nameof(Index));
   }
+
+  public IActionResult Detalhes(int id) {
+    var contato = _context.Contatos.Find(id);
+    
+    if (contato == null)
+      return RedirectToAction(nameof(Index));
+
+    return View(contato);
+  }
 }
