71dc1fad6a115ca5109909f1fc439c42e1793766
Lucas Gretti <lucassgretti@outlook.com>
2023-10-10 01:41:50 -0300

    metodo upload

--- 36-armazenamento-de-dados-na-azure/app/Controllers/ArquivosController.cs
+++ 36-armazenamento-de-dados-na-azure/app/Controllers/ArquivosController.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace app.Controllers
@@ -13,10 +12,29 @@ namespace app.Controllers
         private readonly string _connectionString;
         private readonly string _containerName;
 
+        // Injeção de dependência
         public ArquivosController(IConfiguration configuration)
         {
             _connectionString = configuration.GetValue<string>("BlobConnectionString");
             _containerName = configuration.GetValue<string>("BlobContainerName");
         }
+
+        // POST: /Arquivos/Upload
+        [HttpPost("Upload")]
+        public IActionResult UploadArquivo(IFormFile arquivo) {
+            BlobContainerClient containter = new(_connectionString, _containerName);
+            BlobClient blob = containter.GetBlobClient(arquivo.FileName);
+
+            using var data = arquivo.OpenReadStream();
+            blob.Upload(data, new BlobUploadOptions {
+                HttpHeaders = new BlobHttpHeaders {
+                    ContentType = arquivo.ContentType
+                }
+            });
+
+            return Ok(blob.Uri.ToString());
+        }
+
+
     }
 }
