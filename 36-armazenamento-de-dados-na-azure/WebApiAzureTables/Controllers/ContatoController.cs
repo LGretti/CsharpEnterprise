a0286d93ad20beed99078c6e024c4f5f9b9f1b3d
Lucas Gretti <lucassgretti@outlook.com>
2023-10-10 11:57:24 -0300

    metodo de insert

--- 36-armazenamento-de-dados-na-azure/WebApiAzureTables/Controllers/ContatoController.cs
+++ 36-armazenamento-de-dados-na-azure/WebApiAzureTables/Controllers/ContatoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApiAzureTables.Models;
 
 namespace WebApiAzureTables.Controllers
 {
@@ -20,6 +21,23 @@ namespace WebApiAzureTables.Controllers
             _tabelName        = configuration.GetValue<string>("AzureTableName");
         }
 
-        
+        private TableClient GetTableClient(){
+            var serviceClient = new TableServiceClient(_connectionString); //iniciando o serviço table storage
+            var tabelClient   = serviceClient.GetTableClient(_tabelName);  //iniciando a tabela
+
+            tableClient.CreateIfNotExists(); //ele cria ela no azure
+            return tableClient;
+        }
+
+        [HttpPost]
+        public IACtionResult Criar (Contato contato) {
+            var tableClient = GetTableClient();
+
+            contato.RowKey = Guid.NewGuid().ToString(); //identificador global
+            contato.PartitionKey = contato.RowKey;
+
+            tableClient.UpsertEntity(contato); //substitui o metodo de inserir e atualizar
+            return Ok(contato);
+        }
     }
 }
