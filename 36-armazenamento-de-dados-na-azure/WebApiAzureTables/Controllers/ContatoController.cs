c52e3ec6b3205deff37a3440eaa627db9d7abb07
Lucas Gretti <lucassgretti@outlook.com>
2023-10-10 14:29:12 -0300

    metodo delete

--- 36-armazenamento-de-dados-na-azure/WebApiAzureTables/Controllers/ContatoController.cs
+++ 36-armazenamento-de-dados-na-azure/WebApiAzureTables/Controllers/ContatoController.cs
@@ -70,5 +70,12 @@ namespace WebApiAzureTables.Controllers
 
             return Ok(contatos);
         }
+
+        [HttpDelete("id")]
+        public IActionResult Deletar(string id) {
+            var tableClient = GetTableClient();
+            tableClient.DeleteEntity(id, id);
+            return NoContent();
+        }
     }
 }
