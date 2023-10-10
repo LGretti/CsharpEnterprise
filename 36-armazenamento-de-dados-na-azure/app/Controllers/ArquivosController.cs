f6cc549ff297994ef721599110c84c2e3aead1e4
Lucas Gretti <lucassgretti@outlook.com>
2023-10-10 01:12:33 -0300

    config inicial

--- 36-armazenamento-de-dados-na-azure/app/Controllers/ArquivosController.cs
+++ 36-armazenamento-de-dados-na-azure/app/Controllers/ArquivosController.cs
@@ -10,6 +10,13 @@ namespace app.Controllers
     [Route("[controller]")]
     public class ArquivosController : ControllerBase
     {
-        
+        private readonly string _connectionString;
+        private readonly string _containerName;
+
+        public ArquivosController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("BlobConnectionString");
+            _containerName = configuration.GetValue<string>("BlobContainerName");
+        }
     }
 }
