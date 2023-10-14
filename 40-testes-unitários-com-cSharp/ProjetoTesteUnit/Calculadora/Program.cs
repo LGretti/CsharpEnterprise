ab022195516c30c1c32d56855faf2646e259d539
Lucas Gretti <lucassgretti@outlook.com>
2023-10-14 00:00:31 -0300

    Implementando opracoes simples

--- 40-testes-unitários-com-cSharp/ProjetoTesteUnit/Calculadora/Program.cs
+++ 40-testes-unitários-com-cSharp/ProjetoTesteUnit/Calculadora/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculadora.Services;
+
+CalculadoraImp calc = new CalculadoraImp();
+
+int n1 = 1;
+int n2 = 2;
+
+Console.WriteLine("Soma: " + calc.Somar(n1, n2));
+Console.WriteLine("Subtração: " + calc.Subtrair(n1, n2));
+Console.WriteLine("Multiplicação: " + calc.Multiplicar(n1, n2));
+Console.WriteLine("Divisão: " + calc.Dividir(n1, n2));
---
+++ 40-testes-unitários-com-cSharp/ProjetoTesteUnit/Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int n1, int n2){
+            return n1 + n2;
+        }
+
+        public int Subtrair(int n1, int n2){
+            return n1 - n2;
+        }
+
+        public int Multiplicar(int n1, int n2){
+            return n1 * n2;
+        }
+
+        public int Dividir(int n1, int n2){
+            return n1 / n2;
+        }
+    }
+}
