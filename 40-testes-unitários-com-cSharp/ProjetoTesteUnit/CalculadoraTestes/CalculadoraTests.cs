5b5ca5ad201e165d925c2d2894b0eace32be7eef
Lucas Gretti <lucassgretti@outlook.com>
2023-10-14 10:29:40 -0300

    implementando testes

--- 40-testes-unitários-com-cSharp/ProjetoTesteUnit/CalculadoraTestes/CalculadoraTests.cs
+++ 40-testes-unitários-com-cSharp/ProjetoTesteUnit/CalculadoraTestes/CalculadoraTests.cs
@@ -11,13 +11,17 @@ public class CalculadoraTestes
         _calc = new CalculadoraImp();
     }
 
-    [Fact]
+    [Fact]//indica que é um método de testes
     public void DeveSomar5Com10ERetornar15()
     {
         //Arrange
         int n1 = 5;
         int n2 = 10;
+        
+        //Act
+        int r = _calc.Somar(n1, n2);
 
-        Assert.Equal(15, _calc.Somar(n1, n2));
+        //Assert
+        Assert.Equal(15, r);
     }
 }
